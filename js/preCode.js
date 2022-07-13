691da98d0b07645a9e5534e5874b483997a30ce7
naoto74 <106652928+naoto74@users.noreply.github.com>
2022-07-13 23:01:34 +0900

    短縮

--- js/preCode.js
+++ js/preCode.js
@@ -1,23 +1,19 @@
 (function(){
-var Codetarget = document.getElementById("viewFrame").children;
+var codef = document.getElementById("viewFrame").children;
 var srcHTML = "\n";
-for(var i=0;i<Codetarget.length;i++){
+for(var i=0;i<codef.length;i++){
     var ex = "html";
-    if(Codetarget[i].dataset.file){
-        ex = Codetarget[i].dataset.file.split(".").reverse()[0];
-    }
+    if(codef[i].dataset.file)ex = codef[i].dataset.file.split(".").reverse()[0];
     if(ex=="html"){
-        if(Codetarget[i].TagName!="script"&&Codetarget[i].TagName!="style"){
-            srcHTML += Codetarget[i].outerHTML+"\n";
-        }
+    if(codef[i].TagName!="script"&&codef[i].TagName!="style")srcHTML += codef[i].outerHTML+"\n";
     }else{
-        var pre = document.createElement("pre");
-        var code = document.createElement("code");
-        pre.dataset.lang=ex;
-        pre.dataset.file=Codetarget[i].dataset.file;
-        code.innerText = Codetarget[i].innerText;
-        pre.appendChild(code);
-        document.querySelector(".contentBody").insertAdjacentElement("beforeend",pre);
+    var pre = document.createElement("pre");
+    var code = document.createElement("code");
+    pre.dataset.lang=ex;
+    pre.dataset.file=codef[i].dataset.file;
+    code.innerText = codef[i].innerText;
+    pre.appendChild(code);
+    document.querySelector(".contentBody").insertAdjacentElement("beforeend",pre);
     };
 };
 var pre = document.createElement("pre");
